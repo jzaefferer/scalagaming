b897e350aa2962928e42e721864dd45ecc8ac4c6
joern.zaefferer <joern.zaefferer@5c236da2-4c49-11de-9264-43ebd13e77c3>
2009-07-07 16:08:52 +0000

    removed border obstacles
    git-svn-id: https://scalagaming.googlecode.com/svn/trunk@12 5c236da2-4c49-11de-9264-43ebd13e77c3

--- ScalaGames2d/src/de/bassistance/catman/Field.scala
+++ ScalaGames2d/src/de/bassistance/catman/Field.scala
@@ -6,11 +6,7 @@ import javax.imageio.ImageIO
 class Field(val dimensions: Dimension) {
   
   val burgers = List(new Burger(500, 500), new Burger(1200, 200), new Burger(250, 750), new Burger(1300, 800))
-  val obstructions = List(new Rectangle(0, 0, 10, dimensions.height),
-                          new Rectangle(10, 0, dimensions.width, 10),
-                          new Rectangle(dimensions.width - 10, 0, 10, dimensions.height),
-                          new Rectangle(10, dimensions.height - 10, dimensions.width - 10, dimensions.height),
-                          new Rectangle(152, 50, 75, 120),
+  val obstructions = List(new Rectangle(152, 50, 75, 120),
                           new Rectangle(500, 120, 400, 204),
                           new Rectangle(550, 400, 50, 150),
                           new Rectangle(800, 600, 200, 200))
